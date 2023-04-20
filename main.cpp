7dff3a69e3d3604b5e7110db995bb682ef996e3b
admin <admin@BCS-Ubuntu-32.lrccd.ad.losrios.edu>
2023-04-20 16:24:53 -0700

    done

--- main.cpp
+++ main.cpp
@@ -3,101 +3,131 @@
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include <sstream>
-//test
+
 // Make code easier to type with "using namespace"
 using namespace sf;
 using namespace std;
+
 int main()
 {
-	float width = sf::VideoMode::getDesktopMode().width;
-	float height = sf::VideoMode::getDesktopMode().height;
-	sf::View view(sf::FloatRect(0.f, 0.f, width, height));
-	// Create a video mode object
-	VideoMode vm(width, height);
-	//VideoMode vm(800, 600);
-	// Create and open a window for the game
+    float width = sf::VideoMode::getDesktopMode().width;
+    float height = sf::VideoMode::getDesktopMode().height;
+
+    sf::View view(sf::FloatRect(0.f, 0.f, width, height));
+    // Create a video mode object
+
+    VideoMode vm(width, height);
+    // Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!", Style::Default);
-	vector<Vector2f> vertices;
 	Text messageText;
-	// We need to choose a font
-	Font font;
-	font.loadFromFile("./fonts/Roboto-Black.ttf");
-	messageText.setFont(font);
-	messageText.setCharacterSize(22);
+    // We need to choose a font
+    Font font;    font.loadFromFile("./fonts/Roboto-Black.ttf");    messageText.setFont(font);
+messageText.setCharacterSize(22);
+
+Std:stringstream ss;
+ss<<"Left click 3+ points, then right click to start.";
+messageText.setString(ss.str());int main()
+{
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Chaos Game!", Style::Default);
+
+    vector<Vector2f> vertices;
+    vector<Vector2f> points;
+    int numberOfPoints = 0;
+    int lastvertex = 0;
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+    Text text;
+
+    text.setFont(font); 
+    text.setString("Left click 3-4 points, right click to start");
+    text.setCharacterSize(24); 
+    text.setFillColor(Color::White);
+    text.setStyle(Text::Bold);
+
+
 
 	while (window.isOpen())
 	{
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
-		Event event;
+        
+        Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
+            {
 				window.close();
-			if (event.type == sf::Event::MouseButtonPressed)
-			{
-				if (event.mouseButton.button == sf::Mouse::Left)
-				{
-					std::cout << "the right button was pressed" << std::endl;
-					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+            }
+            if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    cout << "the left button was pressed" << endl;
+                    cout << "mouse x: " << event.mouseButton.x << endl;
+                    cout << "mouse y: " << event.mouseButton.y << endl;
 
-					Vector2i pixelPos = { event.mouseButton.x, event.mouseButton.y };
-					Vector2f worldPos = window.mapPixelToCoords(pixelPos, view);
-					vertices.push_back(worldPos);
+                    numberOfPoints = numberOfPoints + 1;
 
-					//vertices.push_back(Vector2f((float)event.mouseButton.x,
-					//	(float)event.mouseButton.y));
-				}
-			}
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
+                    if(vertices.size() < numberOfPoints)
+                    {
+                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    }
+                }
+                else if (event.mouseButton.button == Mouse::Right)
+                {
+                    if(points.size() == 0)
+                    {
+                        cout << "the right button was pressed" << endl;
+                        cout << "mouse x: " << event.mouseButton.x << endl;
+                        cout << "mouse y: " << event.mouseButton.y << endl;
+                        points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    }
+                }
+            }
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
-		/*
-		****************************************
-		Update the scene
-		****************************************
-		*/
-		// Measure time
-		//Time dt = clock.restart();
+        
 
-		// Update the text
-		std::stringstream ss;
-		ss << "Vertex count = " << vertices.size();
-		messageText.setString(ss.str());
-		FloatRect textRect = messageText.getLocalBounds();
-		messageText.setOrigin(textRect.left +
-			textRect.width / 2.0f,
-			textRect.top +
-			textRect.height / 2.0f);
-		messageText.setPosition(width / 2.0f, 100);
+        if (points.size() > 0)
+        {
 
-		/*
-		****************************************
-		Draw the scene
-		****************************************
-		*/
-		window.setView(view);
-		// Clear everything from the last run frame
-		window.clear();
-		// Draw our game scene here
-		
-		RectangleShape shape{ Vector2f{4,4} };
-		shape.setFillColor(Color::Cyan);
-		for (size_t i = 0; i < vertices.size(); i++)
-		{
-			shape.setPosition(vertices.at(i));
-			window.draw(shape);
-		}
-		window.draw(messageText);
-		// Show everything we just drew
-		window.display();
+            int random; 
+            if (numberOfPoints == 3)
+            {
+                random = rand() % numberOfPoints;
+            }
+            else
+            {
+                do {random = rand() % numberOfPoints;} while (lastvertex == random);
+                lastvertex = random;
+            }
+
+            
+            points.push_back(Vector2f( (vertices[random].x + points[points.size() - 1].x) / 2, (vertices[random].y + points[points.size() - 1].y) / 2));
+        }
+
+        
+        window.clear();
+        window.draw(text);
+        for(int i = 0; i < vertices.size(); i++)
+        {
+            RectangleShape rect(Vector2f(15,15));
+            rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+            rect.setFillColor(Color::Red);
+            window.draw(rect);
+            
+  
+        }
+        for (int i = 0; i < points.size(); i++)
+        {
+            RectangleShape rect(Vector2f(7, 7));
+            rect.setPosition(Vector2f(points[i].x, points[i].y));
+            rect.setFillColor(Color::White);
+            window.draw(rect);
+        }
+        
+        window.display();
 	}
-
-	return 0;
 }
